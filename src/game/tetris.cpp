43c2aab73b2da70b3d9b86b5ca05dd8565496139
d-- <d--@users.noreply.github.com>
2020-04-21 10:20:58 -0400

    Rendering Example Text
    This was just to check the box.  Now that I'm at the point of needing to
    create some entity abstraction, I can start to worry about the overall
    abstractions and structure that I'm going to need.  I just need to try
    not to YAGNI myself.

--- src/game/tetris.cpp
+++ src/game/tetris.cpp
@@ -1,5 +1,6 @@
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_ttf.h>
 #include <cstdlib>
 #include <stack>
 #include <deque>
@@ -29,8 +30,18 @@ int main(int argc, char *argv[]) {
     loaded = IMG_Init(imgflags);
     if ((loaded & imgflags) != imgflags) {
         SDL_Log("Could not init SDL_image: %s\n", IMG_GetError());
+        SDL_Quit();
+        return 1;
     }
     atexit(IMG_Quit);
+
+    loaded = TTF_Init();
+    if (loaded == -1) {
+        SDL_Log("Could not init SDL_ttf: %s\n", TTF_GetError());
+        SDL_Quit();
+        return 1;
+    }
+    atexit(TTF_Quit);
 
     Uint32 windowFlags;
     windowFlags = SDL_WINDOW_RESIZABLE | SDL_WINDOW_SHOWN;
@@ -61,6 +72,34 @@ int main(int argc, char *argv[]) {
 
     assetManager.loadImages();
     std::unordered_map<std::string, SDL_Texture *> textures;
+
+    TTF_Font *font = TTF_OpenFont("assets/Philosopher-Regular.ttf", 28);
+    if (font == nullptr) {
+        SDL_Log("Could not load font: %s\n", TTF_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Surface *textSurface = TTF_RenderText_Blended(font,
+        "Successfully displaying text!",
+        SDL_Color{Uint8(0xFF), Uint8(0xFF), Uint8(0xFF), Uint8(0xFF)});
+    if (textSurface == nullptr) {
+        SDL_Log("Could not render text surface: %s\n", TTF_GetError());
+        SDL_Quit();
+        return 1;
+    }
+    SDL_Rect textDest{0, 0, textSurface->w, textSurface->h};
+    textDest.x = (400 - textDest.w) / 2;
+    textDest.y = (400 - textDest.h) / 2;
+
+    SDL_Texture *textTexture = SDL_CreateTextureFromSurface(renderer,
+        textSurface);
+    if (textTexture == nullptr) {
+        SDL_Log("Could not convert text surface: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+    SDL_FreeSurface(textSurface);
 
     Uint32 currentTimeMs, elapsedTimeMs = 0;
     Uint32 previousTimeMs = SDL_GetTicks();
@@ -105,6 +144,7 @@ int main(int argc, char *argv[]) {
                     nullptr, nullptr);
             }
         }
+        SDL_RenderCopy(renderer, textTexture, nullptr, &textDest);
         // SDL_RenderCopy(renderer, texture, src, &dst);
         SDL_RenderPresent(renderer);
     }
