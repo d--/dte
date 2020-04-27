7aa77a18ebf13e50074641b57bc70c6594242894
d-- <d--@users.noreply.github.com>
2020-04-27 11:05:19 -0400

    Fix Expand Contract Direction
    I needed to remove half the scaling delta from the position, otherwise
    it looked like the block was always expanding in the down-right
    direction and contracting in the opposite.

--- src/game/entities/first/block_draw.cpp
+++ src/game/entities/first/block_draw.cpp
@@ -17,7 +17,7 @@ namespace dte {
     void BlockDrawComponent::update(Entity &entity, SDL_Renderer *renderer, 
             Uint32 totalTimeMs, float remainderFrames) {
         float time = float(totalTimeMs) + (FIXED_MS_UPDATE * remainderFrames);
-        float slow = 400.f;
+        float slow = 200.f;
         float scale = 20.f;
         int scaleWDelta = int(sin((time * (PI/2)) / slow) * scale);
         int scaleHDelta = int(sin((time * (PI/2)) / 400.f) * 20.f);
@@ -26,8 +26,8 @@ namespace dte {
         rect.h = 128 + scaleHDelta;
         float xDelta = blockTransformComponent->getXDelta();
         float yDelta = blockTransformComponent->getYDelta();
-        rect.x = int(blockTransformComponent->getX() + (xDelta * remainderFrames));
-        rect.y = int(blockTransformComponent->getY() + (yDelta * remainderFrames));
+        rect.x = int(blockTransformComponent->getX() + (xDelta * remainderFrames)) - (scaleWDelta / 2);
+        rect.y = int(blockTransformComponent->getY() + (yDelta * remainderFrames)) - (scaleHDelta / 2);
         SDL_RenderCopyEx(renderer, texture, nullptr, &rect, rotDelta,
             nullptr, SDL_FLIP_NONE);
     }
